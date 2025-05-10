bdb63abffaf082c01256a1a301cbbce395f9f6fd
Rafael Feitosa <rafaelfeit@hotmail.com>
2025-05-09 23:35:36 -0300

    atualizacao arquivo timedelta

--- 04 - Data e hora/02_timedelta.py
+++ 04 - Data e hora/02_timedelta.py
@@ -1,25 +1,25 @@
 from datetime import date, datetime, timedelta
 
-tipo_carro = "M"  # P, M, G
+tipo_carro = "P"  # P, M, G
 tempo_pequeno = 30
 tempo_medio = 45
 tempo_grande = 60
 data_atual = datetime.now()
 
 if tipo_carro == "P":
-    data_estimada = data_atual - timedelta(days=tempo_pequeno)
-    print(f"O carro chegou: {data_atual} e ficará pronto às {data_estimada}")
+    data_estimada = data_atual + timedelta(minutes=tempo_pequeno)
+    print(f"O carro chegou: {data_atual.strftime("%d/%m/%Y %H:%M:%S")} e ficará pronto às {data_estimada.strftime("%d/%m/%Y %H:%M:%S")}")
 elif tipo_carro == "M":
-    data_estimada = data_atual - timedelta(days=tempo_medio)
-    print(f"O carro chegou: {data_atual} e ficará pronto às {data_estimada}")
+    data_estimada = data_atual + timedelta(minutes=tempo_medio)
+    print(f"O carro chegou: {data_atual.strftime("%d/%m/%Y %H:%M:%S")} e ficará pronto às {data_estimada.strftime("%d/%m/%Y %H:%M:%S")}")
 else:
-    data_estimada = data_atual - timedelta(days=tempo_grande)
-    print(f"O carro chegou: {data_atual} e ficará pronto às {data_estimada}")
+    data_estimada = data_atual + timedelta(minutes=tempo_grande)
+    print(f"O carro chegou: {data_atual.strftime("%d/%m/%Y %H:%M:%S")} e ficará pronto às {data_estimada.strftime("%d/%m/%Y %H:%M:%S")}")
 
 
 print(date.today() - timedelta(days=1))
 
-resultado = datetime(2023, 7, 25, 10, 19, 20) - timedelta(hours=1)
+resultado = datetime(2025, 5, 9, 10, 19, 20) - timedelta(hours=1)
 print(resultado.time())
 
 print(datetime.now().date())
